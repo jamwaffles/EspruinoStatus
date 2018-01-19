7c025a87555977c923728e0306655dac6ca33b54
James Waples <jamwaffles@gmail.com>
2018-01-19 20:19:43 +0000

    WIP Pico code

---
+++ pico.js
@@ -0,0 +1,23 @@
+// function start(){
+// 	// write some text
+// 	g.drawString("Hello World!",2,2);
+// 	// write to the screen
+// 	g.flip();
+// }
+
+// // SPI
+// var s = new SPI();
+// s.setup({mosi: B15 /* D1 */, sck:B13 /* D0 */});
+// var g = require("SSD1306").connectSPI(s, B14 /* DC */, B10 /* RST - can leave as undefined */, start);
+
+// function start_mono(){
+// 	// write some text
+// 	g_mono.drawString("Hello World!",2,2);
+// 	// write to the screen
+// 	g_mono.flip();
+// }
+
+// // I2C
+// I2C1.setup({scl:B6,sda:B7});
+// var g_mono = require("SSD1306").connect(I2C1, start_mono);
+
