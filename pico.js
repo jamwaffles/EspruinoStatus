d0ac857400afa8520e6f997a41e60b859f6901cb
James Waples <jamwaffles@gmail.com>
2018-01-19 21:48:31 +0000

    Connect to a websocket and display the time

--- pico.js
+++ pico.js
@@ -1,45 +1,86 @@
-// function start(){
-// 	// write some text
-// 	g.drawString("Hello World!",2,2);
-// 	// write to the screen
-// 	g.flip();
-// }
-
-// // SPI
-// var s = new SPI();
-// s.setup({mosi: B15 /* D1 */, sck:B13 /* D0 */});
-// var g = require("SSD1306").connectSPI(s, B14 /* DC */, B10 /* RST - can leave as undefined */, start);
-
 // function start_mono(){
-// 	// write some text
-// 	g_mono.drawString("Hello World!",2,2);
-// 	// write to the screen
-// 	g_mono.flip();
+//  // write some text
+//  g_mono.drawString("Hello World!",2,2);
+//  // write to the screen
+//  g_mono.flip();
 // }
 
 // // I2C
 // I2C1.setup({scl:B6,sda:B7});
 // var g_mono = require("SSD1306").connect(I2C1, start_mono);
 
-var esp = require("https://gist.githubusercontent.com/jamwaffles/5141e1a4a1c6258b81831b5315074336/raw/355f74ecc8981c5da778980511f86bdf1daa8c18/esp8266_0v25_fixed.js");
+var WebSocket = require("ws");
 
-digitalWrite(B9,1);
+function connectToWifi(cb) {
+  var esp = require("https://raw.githubusercontent.com/jamwaffles/EspruinoStatus/master/esp8266_0v25_hacked.js");
 
-Serial2.setup(115200, { rx: A3, tx : A2 });
+  digitalWrite(B9,1);
 
-var wifi = esp.connect(Serial2);
+  Serial2.setup(115200, { rx: A3, tx : A2 });
 
-wifi.reset(function(e) {
+  var wifi = esp.connect(Serial2);
 
-  console.log("Connecting to WiFi");
-  wifi.connect("ClownsUnderTheBed","3dooty5g", function(e) {
-//    if (e) throw e;
-    console.log("Connected");
-    wifi.getIP(console.log);
-    require("http").get("http://www.espruino.com", function(res) {
-      console.log("Response: ",res);
-      res.on('data', function(d) {
-        console.log("--->"+d);
+  wifi.reset(function(e) {
+    wifi.connect("ClownsUnderTheBed","3dooty5g", function(e) {
+      cb();
+    });
+  });
+}
+
+function connectToWebsocket(cb) {
+  var host = '192.168.0.8';
+
+  var ws = new WebSocket(host, { port: 8080 });
+
+  ws.on('open', function() {
+    console.log("Connected to server");
+
+    cb(ws);
+  });
+}
+
+function connectToDisplay(cb) {
+  var s = new SPI();
+  s.setup({mosi: B6 /* D1 */, sck:B5 /* D0 */});
+  var g = require("SSD1306").connectSPI(s, A8 /* DC */, B7 /* RST - can leave as undefined */, function() {
+    console.log("Noice");
+    cb(g);
+  });
+}
+
+function drawTime(d, time){
+  d.clear();
+  // write some text
+  d.drawString("Da tiem:",2,2);
+
+  d.drawString(time,2,20);
+  // write to the screen
+  d.flip();
+}
+
+console.log("Starting");
+
+connectToDisplay(function(disp) {
+  console.log("Connected to display");
+
+  disp.clear();
+  disp.drawString("Connecting...",2,2);
+  disp.flip();
+
+  connectToWifi(function() {
+    console.log("Connected to wifi");
+    disp.clear();
+    disp.drawString("Opening socket...",2,2);
+    disp.flip();
+
+    connectToWebsocket(function(ws) {
+      console.log("All ready");
+      disp.clear();
+      disp.drawString("Waiting for messages...",2,2);
+      disp.flip();
+
+      ws.on('message', function(msg) {
+        drawTime(disp, msg);
       });
     });
   });
