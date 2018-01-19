e12b0221e6482a287589e6f16cd4594976692ce2
James Waples <jamwaffles@gmail.com>
2018-01-19 20:19:31 +0000

    Basic socket server

---
+++ index.js
@@ -0,0 +1,13 @@
+import 'isomorphic-fetch'
+import WebSocket from 'ws'
+
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
